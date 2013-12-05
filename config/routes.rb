33ba47ee645f84e8a2921d4002de6e1a2552c60c
Jared Smith <jaredsmithse@icloud.com>
2013-12-04 23:25:55 -0800

    Adding in all files so I can pull everything onto new computer.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Raffler::Application.routes.draw do
-  resources :entries
-
+  scope "api" do 
+    resources :entries
+  end
+  
   root to: "main#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
